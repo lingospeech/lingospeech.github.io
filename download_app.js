ef635001a7e51c07ab7e87a65295cff85b5d4ae6
lingospeech <143069734+lingospeech@users.noreply.github.com>
2023-12-24 15:25:05 +0100

    Update download_app.js

--- download_app.js
+++ download_app.js
@@ -2,15 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.getElementById('downloadButton');
     downloadButton.addEventListener('click', function() {
       const downloadLink = document.createElement('a');
-      downloadLink.href = 'https://github.com/symphonious/manylanguages/releases/download/streamlanguages-1-0-0/streamlanguages.zip'; 
-      downloadLink.download = 'streamlanguages.zip'; 
+      downloadLink.href = 'https://github.com/symphonious/manylanguages/releases/download/streamlanguages-1-0-1/streamlanguages-win64.zip'; 
+      downloadLink.download = 'streamlanguages-win64.zip'; 
 
       downloadLink.click();
       console.log("download");
-      const websocketClient2 = new WebSocket("wss://symphonious.glitch.me/");
-      websocketClient2.onopen = function(event) {
-        websocketClient2.send("downloaded");  
-      };
+      
     });
 });
 
