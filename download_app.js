89cbe5915720bfecfec498bfa27c751e6062bb8b
lingospeech <143069734+lingospeech@users.noreply.github.com>
2024-01-01 10:31:45 +0100

    Update download_app.js

--- download_app.js
+++ download_app.js
@@ -1,3 +1,10 @@
+
+const url = 'http://symphonious.glitch.me/webping';
+const data = {
+  key1: 'value1',
+  key2: 'value2'
+};
+
 document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.getElementById('downloadButton');
     downloadButton.addEventListener('click', function() {
@@ -7,10 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
       downloadLink.click();
       console.log("download");
-      const websocketClient2 = new WebSocket("wss://symphonious.glitch.me/");
-      websocketClient2.onopen = function(event) {
-        websocketClient2.send("downloaded");  
-      };
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify(data), 
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json(); 
+        })
+        .then(data => {
+          console.log('response:', data);
+        })
+        .catch(error => {
+          console.error('Error request:', error);
+        });
+
+
+        
       
     });
 });
